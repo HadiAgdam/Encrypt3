1e460836bd4060d2e71942b74af1fc15f1834751
Hadi Agdam <97987496+HadiAgdam@users.noreply.github.com>
2024-06-29 05:11:20 -0700

    initial commit

---
+++ Encrypter.py
@@ -0,0 +1,139 @@
+from random import randint
+
+symbols = "! @ # $ % ^ & *".split(" ")
+
+li = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)] + [str(i) for i in range(0, 10)] + symbols
+
+li_old = [chr(i) for i in range(97, 123)] + [str(i) for i in range(0, 10)]
+
+
+def get_old(v: int):
+    return li_old[v % len(li_old)]
+
+
+def get(v: int):
+    return li[v % len(li)]
+
+
+def get_index(e):
+    for i in range(0, len(li)):
+        if e == li[i]:
+            return i
+
+
+class Encrypter:
+    keys = []
+
+
+    def get3(self, i, r1, r2, l):
+        x = i
+
+        
+        # for k in self.keys:
+            # x += k
+
+        for j in range(len(self.keys)):
+            if j % 2 == 0:
+                x += (self.keys[j] + 1) * r1
+            else:
+                x -= (self.keys[j] + 1) * r2
+        
+        x += l
+            
+   
+        return li[x % len(li)]
+    
+
+    def get3_o(self, i, r1, r2, l):
+        x = i
+
+        x -= l
+
+        for j in range(len(self.keys) - 1, -1, -1):
+            if j % 2 == 0:
+                x -= int((self.keys[j] + 1) * r1)
+            else:
+                x += (self.keys[j] + 1) * r2
+        
+        return li[x % len(li)]
+
+    
+
+
+    def initKeys(self, key_path: str):
+        f = open(key_path)
+
+        s = f.read()
+
+        for t in s:
+            self.keys.append(ord(t) * self.key)
+
+
+    def __init__(self, key: str, key_path: str):
+        a = 0
+        for i in key:
+            a += ord(i)
+        self.key = a * len(key) + 1
+        
+        self.initKeys(key_path)
+
+
+
+    def encrypt(self, text: str, r1: int = -1, r2: int = -1):
+        if r1 == -1:
+            r1 = randint(0, len(li) - 1)
+        if r2 == -1:
+            r2 = randint(0, len(li) - 1)
+        
+        result = get(r1)
+
+        for j in range(0, len(text)):
+            t = text[j]
+            i = get_index(t)
+            if not i:
+                result += t
+                continue
+            result += self.get3(i, r1, r2, j + 1)
+        
+        result += get(r2)
+
+
+        return result, r1, r2
+
+    def decrypt(self, text: str):
+
+        # Encrypt 1
+        result1 = ""
+        if text.lower() == text:  # it means using old method
+            for i in range(0, len(text)):
+                if li_old.__contains__(text.lower()[i]):
+                    result1 += get_old(li_old.index(text.lower()[i]) - self.key - i)
+                else:
+                    result1 += text[i]
+
+
+        # Encrypt 2
+        result2 = ""
+        r1 = get_index(text[0])
+        r2 = get_index(text[len(text) - 1])
+        for i in range(1, len(text) - 1):
+            t = text[i]
+            j = get_index(t)
+            if not j:
+                result2 += t
+                continue
+            result2 += get(j - r1 - self.key + r2 - i + 1)
+
+
+        # Encrypt 3
+        result3 = ""
+        for i in range(1, len(text) - 1):
+            t = text[i]
+            j = get_index(t)
+            if not j:
+                result3 += t
+                continue
+            result3 += self.get3_o(j, r1, r2, i)
+        
+
+        return result1, result2, result3
---
+++ main.py
@@ -0,0 +1,59 @@
+from Encrypter import Encrypter
+
+# key
+
+if __name__ == "__main__":
+
+    i = input("key, path: ").split(' ')
+    path = "key"
+
+    if len(i) == 2:
+        path = i[1]
+
+    e = Encrypter(i[0], path)
+    print()
+    while True:
+        command = input("command :")
+
+
+        if command.startswith("ex"):
+            r1 = int(command.split(" ")[1])
+            r2 = int(command.split(" ")[2])
+
+            i = 5 + len(command.split(" ")[1]) + len(command.split(" ")[2])
+
+            print()
+            en = e.encrypt(command[i:], r1, r2)
+            print(en[0])
+            print(en[1], en[2])
+            print()
+
+        elif command.startswith("e"):
+            print()
+            en = e.encrypt(command[2:])
+            print(en[0])
+            print(en[1], en[2])
+            print()
+
+        elif command.startswith("d"):
+            print()
+            x = e.decrypt(command[2:])
+            print("Encrypt1 :")
+            print(x[0])
+
+            print()
+
+            print("Encrypt2 :")
+            print(x[1])
+
+            print()
+
+
+            print("Encrypt3 :")
+            print(x[2])
+
+            print()
+
+
+        else:
+            print("command not found")
